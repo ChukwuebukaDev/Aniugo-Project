ce825d2150a423082251de1a172854753b1d8773
ChukwuebukaDev <ebuzuaniugo@yahoo.com>
2025-06-27 13:15:19 +0100

    make the cart quantity interactive

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -72,8 +72,11 @@ document.querySelectorAll('.add-btn').forEach((button)=>{
             quantity:1
         });
     }
-        console.log(cart)
-        
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity += item.quantity;
+        })
+        document.querySelector('.cart-quantity').textContent = cartQuantity;
     })
 })
 
